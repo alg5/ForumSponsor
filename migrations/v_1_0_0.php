cb2113125b5e2f3b8d52316d0edb3efc2894e1fd
alg5 <chgk.globus@gmail.com>
2015-06-29 22:03:05 +0300

    remove comment lines

--- migrations/v_1_0_0.php
+++ migrations/v_1_0_0.php
@@ -28,7 +28,6 @@ class v_1_0_0 extends \phpbb\db\migration\migration
 				'add_columns' => array (
 					$this->table_prefix . 'forums' => array  (
 						'forum_sponsor' => array('VCHAR:500', ''),
-						//'forum_sponsor_allow_html' => array('BOOL', 0),
 						'forum_sponsor_parse_type' => array('BOOL', 0),
 						'forum_sponsor_uid' => array('VCHAR:8', ''),
 						'forum_sponsor_bitfield' => array('VCHAR:255', ''),
@@ -36,7 +35,6 @@ class v_1_0_0 extends \phpbb\db\migration\migration
 
 						'forum_sponsor_above' => array('VCHAR_UNI:500', ''),
 						'forum_sponsor_above_parse_type' => array('BOOL', 0),
-						//'forum_sponsor_above_allow_html' => array('BOOL', 0),
 						'forum_sponsor_above_uid' => array('VCHAR:8', ''),
 						'forum_sponsor_above_bitfield' => array('VCHAR:255', ''),
 						'forum_sponsor_above_options' => array('UINT:11', 7),
@@ -49,7 +47,6 @@ class v_1_0_0 extends \phpbb\db\migration\migration
 	{
 		return array(
 			'drop_columns'	=> array(
-//					$this->table_prefix . 'forums' => array('forum_sponsor'),
 					$this->table_prefix . 'forums' => array(
 							'forum_sponsor',
 							'forum_sponsor_parse_type',

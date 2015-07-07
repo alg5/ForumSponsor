7bc0f179c74e1ceaacf92815298cb86365e61f04
alg5 <chgk.globus@gmail.com>
2015-07-07 22:36:30 +0300

    fix html

--- styles/all/template/ForumSponsor.js
+++ styles/all/template/ForumSponsor.js
@@ -5,7 +5,7 @@
 
     $('.sponsor').each(function () {
         var forumtitle = $(this).next().find('div.list-inner');
-        var div = '<div class="forumsponsor" style="float:' + S_CONTENT_FLOW_END + '; margin-' + S_CONTENT_FLOW_END + ': 5px;">' + L_FORUM_SPONSOR + ':<br><p class="sponsoright">' + $(this).attr('data-sponsor') + '</p></div>';
+        var div = '<div class="forumsponsor" style="float:' + S_CONTENT_FLOW_END + '; margin-' + S_CONTENT_FLOW_END + ': 5px;">' + L_FORUM_SPONSOR + ':<br /><p class="sponsoright">' + $(this).attr('data-sponsor') + '</p></div>';
         $(forumtitle).html(div + $(forumtitle).html());
     });
 
